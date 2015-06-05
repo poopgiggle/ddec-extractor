34e009aad504109888d58bb238320324d384f2b9
poopgiggle <poopgiggle@gmail.com>
2015-06-05 11:48:33 -0500

    test cases

--- test_parse.py
+++ test_parse.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python2
-
+import json
+import base64
 import data_defs
 
-f = open('hard-brake.bin','rb')
-message = f.read()
+f = open('ddec1587-ft-lauderdale.json','r')
+data_dict = json.load(f)
 
-parsed_data = data_defs.parse_message(message)
+for key in data_dict.keys():
+    print("Parsing %s" % key)
+    this_page = data_dict[key]
+    raw_page = base64.b64decode(this_page)
+    print(data_defs.parse_message(raw_page))
